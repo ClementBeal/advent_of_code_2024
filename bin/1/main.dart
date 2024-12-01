9398e79f7361bfa9a9505b495eaf07d338524513
Clement <clement.beal@outlook.com>
2024-12-01 09:28:12 -0300

    day 1: part 2

--- bin/1/main.dart
+++ bin/1/main.dart
@@ -6,21 +6,45 @@ void main() {
   final firstList = <int>[];
   final secondList = <int>[];
 
+  // current location id - counter value
+  final firstCounter = <int, int>{};
+  final secondCounter = <int, int>{};
+
   for (final line in inputData) {
     final tokens = line.split("   ").map(int.parse).toList();
 
-    firstList.add(tokens[0]);
-    secondList.add(tokens[1]);
+    final firstValue = tokens[0];
+    final secondValue = tokens[1];
+
+    firstList.add(firstValue);
+    secondList.add(secondValue);
+
+    firstCounter.update(
+      firstValue,
+      (value) => value + 1,
+      ifAbsent: () => 1,
+    );
+    secondCounter.update(
+      secondValue,
+      (value) => value + 1,
+      ifAbsent: () => 1,
+    );
   }
 
   firstList.sort();
   secondList.sort();
 
   int distance = 0;
+  int similarityScore = 0;
 
   for (int i = 0; i < inputData.length; i++) {
     distance += (firstList[i] - secondList[i]).abs();
+
+    final score = firstList[i] * (secondCounter[firstList[i]] ?? 0);
+
+    similarityScore += score;
   }
 
   print("Total distance : $distance");
+  print("Similarity score : $similarityScore");
 }
