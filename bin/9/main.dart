a223190ac9962195e01d948646cf258ae2b13d55
Clement <clement.beal@outlook.com>
2024-12-09 18:51:09 -0300

    day 9 part 2

--- bin/9/main.dart
+++ bin/9/main.dart
@@ -5,6 +5,7 @@ Future<void> main(List<String> args) async {
   final input = File(r"inputs/9.txt").readAsStringSync();
 
   print(calculateChecksum(input));
+  print(calculateChecksumWithoutFragmentation(input));
 }
 
 int calculateChecksum(String disk) {
@@ -55,6 +56,99 @@ int calculateChecksum(String disk) {
   int checksum = 0;
   for (var i = 0; i < rightCursor + 1; i++) {
     checksum += list[i] * i;
+  }
+
+  return checksum;
+}
+
+int calculateChecksumWithoutFragmentation(String disk) {
+  int max = 64000;
+  int total = 0;
+
+  for (int i = 0; i < disk.length; i++) {
+    total += int.parse(disk[i]);
+  }
+
+  final original = Uint16List(disk.length);
+  final list = Uint16List(total);
+
+  int j = 0;
+
+  bool isBlock = true;
+  int blockId = 0;
+
+  for (var i = 0; i < disk.length; i++) {
+    final int value = int.parse(disk[i]);
+    original[i] = value;
+
+    final limit = value + j;
+
+    if (isBlock) {
+      for (; j < limit; j++) {
+        list[j] = blockId;
+      }
+      blockId++;
+    } else {
+      for (; j < limit; j++) {
+        list[j] = max;
+      }
+    }
+
+    isBlock = !isBlock;
+  }
+
+  int leftCursor = 0;
+  int rightCursor = list.length - 1;
+
+  while (0 < rightCursor) {
+    leftCursor = 0;
+
+    while (list[rightCursor] == max) {
+      rightCursor--;
+      if (rightCursor < 0) break;
+    }
+
+    int currentValue = list[rightCursor];
+
+    int length = 0;
+
+    while (list[rightCursor - length] == currentValue) {
+      length++;
+      if (rightCursor - length < 0) break;
+    }
+
+    int freeSpace = 0;
+
+    while (freeSpace < length && leftCursor < rightCursor) {
+      if (list[leftCursor] == max) {
+        freeSpace++;
+      } else {
+        freeSpace = 0;
+      }
+
+      leftCursor++;
+    }
+    leftCursor--;
+
+    if (freeSpace == length && leftCursor < rightCursor) {
+      for (var i = 0; i < length; i++) {
+        list[leftCursor - i] = currentValue;
+      }
+
+      for (var i = 0; i < length; i++) {
+        list[rightCursor - i] = max;
+      }
+    }
+
+    rightCursor -= length;
+  }
+
+  int checksum = 0;
+
+  for (var i = 0; i < list.length - 1; i++) {
+    if (list[i] != max) {
+      checksum += list[i] * i;
+    }
   }
 
   return checksum;
--- test/9_test.dart
+++ test/9_test.dart
@@ -4,15 +4,15 @@ import '../bin/8/main.dart';
 import '../bin/9/main.dart';
 
 void main() {
-  // test("day 9 part 1", () {
-  //   final input = "12345";
-
-  //   expect(calculateChecksum(input), 60);
-  // });
-
-  test("day 9 part 1 -2", () {
+  test("day 9 part 1", () {
     final input = "2333133121414131402";
 
     expect(calculateChecksum(input), 1928);
+  });
+
+  test("day 9 part 2", () {
+    final input = "2333133121414131402";
+
+    expect(calculateChecksumWithoutFragmentation(input), 2858);
   });
 }
